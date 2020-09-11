066ffc0971faac74d501763c6ef8faf64c8ff6a2
josherias <josherias10@gmail.com>
2020-09-11 19:05:38 +0300

    Updates

---
+++ precache-manifest.72e9518352683c79c8b8686c44b3fc32.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce03c80fd19e828c6c5e79a689a6aeb3",
+    "url": "/react-ecommerce-app/index.html"
+  },
+  {
+    "revision": "8ed023c8a70c41778633",
+    "url": "/react-ecommerce-app/static/css/2.ba11b931.chunk.css"
+  },
+  {
+    "revision": "7d4e2086a7f3eaf389ff",
+    "url": "/react-ecommerce-app/static/css/main.62d56404.chunk.css"
+  },
+  {
+    "revision": "8ed023c8a70c41778633",
+    "url": "/react-ecommerce-app/static/js/2.95359177.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-ecommerce-app/static/js/2.95359177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4e2086a7f3eaf389ff",
+    "url": "/react-ecommerce-app/static/js/main.fae114cd.chunk.js"
+  },
+  {
+    "revision": "5df60bdb085757d3fd2e",
+    "url": "/react-ecommerce-app/static/js/runtime-main.2396355a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-ecommerce-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-ecommerce-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-ecommerce-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-ecommerce-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-ecommerce-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2146e6203c9da5a9c25898e6b6b0b5ad",
+    "url": "/react-ecommerce-app/static/media/wallpaper1.2146e620.jpg"
+  },
+  {
+    "revision": "dc5d4c1b9339a575f000dd6f1b7df536",
+    "url": "/react-ecommerce-app/static/media/wallpaper2.dc5d4c1b.jpg"
+  }
+]);
